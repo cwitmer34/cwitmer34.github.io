5e156d0bac33cc4ef5e12c6b8a1bf256e7b807ef
cwitmer34 <connorwitmer34@gmail.com>
2023-03-06 10:32:35 -0500

    hacker text

--- src/Assets/Pages/Navbar.jsx
+++ src/Assets/Pages/Navbar.jsx
@@ -1,14 +1,40 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import arrow from "../Media/arrow.svg";
 import hamburger from "../Media/hamburger.svg";
 import "../Styles/Navbar.css";
 const Navbar = () => {
+  let original = "CONNOR WITMER";
+  let [name, setName] = useState(original);
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return (
     <nav className="nav-container soft">
       <div className="inactive">
         <div className="name">
           <Link to={"/"}>
-            <h2>CONNOR WITMER</h2>
+            <h2
+              onMouseEnter={() => {
+                let iterations = 0;
+                let interval = setInterval(() => {
+                  let modified = name
+                    .split("")
+                    .map((letter, i) => {
+                      if (i < iterations) {
+                        return original[i];
+                      }
+
+                      return alphabet[Math.floor(Math.random() * 26)];
+                    })
+                    .join("");
+
+                  setName(modified);
+                  if (iterations >= original.length) clearInterval(interval);
+                  iterations += 1 / 10;
+                }, 10);
+              }}
+            >
+              {name}
+            </h2>
             <p>full-stack dev</p>
           </Link>
           {/* <img
